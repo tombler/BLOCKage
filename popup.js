ab427bfa535297e0b0e1c16cc26a4f69a405eedb
Tom Griffey <tgriffey9389@gmail.com>
2017-05-10 17:40:11 -0700

    Moving vendor files to vendor/. Added communication between background and popup for user auth, getting user apps, adding an app. Added HTML for adding and displaying apps.

--- popup.js
+++ popup.js
@@ -1,90 +1,47 @@
-// This is all we need if we want the functionality of the CMS in the extension.
-// To implement this instead of ajax, uncomment basicExtension() in setDomContent() && comment out searchButton click function.
-// function basicExtension () {
-//     var frame = document.createElement('iframe');
-//     frame.setAttribute('width', '500px');
-//     frame.setAttribute('height', '500px');
-//     frame.setAttribute('frameborder', '0');
-//     frame.setAttribute('id', 'search-results');
-//     frame.setAttribute('src', 'http://cmsdart.local:8888/app');
-//     document.body.appendChild(frame);
-// }
-
-// dom variables
-function setDomContent(labelText) {
-
-  // basicExtension();
-  // var status = document.getElementById('status');
-  var translateButton = document.getElementById('translate');
-  var input = document.getElementById('textToTranslate');
-  
-  // status.textContent = labelText;
-  translateButton.addEventListener('click', function (e) {
-        emptyResults();
-        xhr(input.value);
-  });
-}
-
-function emptyResults() {
+document.addEventListener('DOMContentLoaded', function () {
     
-    var output = document.getElementById('output');
-    if (output) {
-        document.body.removeChild(output);
-    }
-}
+    chrome.extension.onMessage.addListener(function(message, messageSender, sendResponse) {
+        if (!message.hasOwnProperty('message')) {
+            document.getElementById('myapps').innerHTML = 'Sorry! Something went wrong!';
+            return
+        }
 
-function xhr(searchText) {
-
-    var data = null;
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    // Add event listener for error message
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === 4) {
-        parseResults(this.responseText);
-        return;
-      }
+        if (message.message === 'getApps' && message.status === 'success') {
+            var apps = message.data;
+            if (apps.length > 0) {
+                var html=''
+                for (var i = 0; i < apps.length; i++) {
+                    html += '<div><b>Name:</b> '+apps[i].name+'</div>'
+                    html += '<div><b>URL:</b> '
+                    html += '<a target="_blank" href="https://'+apps[i].url+'">'
+                    html += apps[i].url+'</a></div>'
+                    html += '<hr>'
+                };
+                document.getElementById('myapps').innerHTML = html;
+            } else {
+                var html = '<div><b>You are not tracking any sites right now.<br>Try adding one!</b></div>';
+                document.getElementById('myapps').innerHTML = html;
+            }
+        }
+        else if (message.message === 'addApp' && message.status === 'success') {
+            console.log(message);
+            chrome.runtime.sendMessage({msg:'getApps'});
+            document.getElementById('myAppsAnchor').click();
+        }
+        // else message.status !== 'success', error handle
+        
     });
 
-    var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160415T025414Z.f02fe494eff89f99.fd64e2a5a6ffcf9c6d5db5be89ffee9aa4ddfeea&text=" + searchText + "&lang=en-es";
+    chrome.runtime.sendMessage({msg:'getApps'});
 
-    xhr.open("GET", url);
-    xhr.setRequestHeader("cache-control", "no-cache");
-    xhr.send(data);
-}
+    var addApp = document.getElementById("addApp");
+    addApp.onclick = function(e) { 
+        e.preventDefault();
+        var data = {
+            name: document.getElementById("site_name").value,
+            url: document.getElementById("site_url").value
+        }
+        chrome.runtime.sendMessage({msg:'addApp',data: data});
+    };
 
-
-function parseResults(response) {
-    var responseObj = JSON.parse(response);
-    // Put search results into popup DOM, hidden
-    // var el = document.getElementById('results-hidden');
-    // el.innerHTML = response;
-    // // Search popup DOM for all search results, build html for results to display
-    // var displayHtml = "";
-    // var results = document.querySelectorAll(".search-result");
-    // for (var i=0; i<results.length; i++) {
-    //     displayHtml += "<div style='border: 3px solid black; margin: 10px; height=100px;'>";
-    //     displayHtml += results[i].innerHTML;
-    //     displayHtml += "</div>"
-    // }
-    showResults(responseObj.text);
-    saveResults(responseObj);
-}
-
-function showResults(translatedText) {
-    var resultsEl = document.createElement('div');
-    resultsEl.setAttribute('id', 'output');
-    resultsEl.innerHTML = translatedText;
-    document.body.appendChild(resultsEl);
-}
-
-function saveResults(results) {
-    // Open a port to send saved results to background.js
-    var port = chrome.runtime.connect({name:"savedResults"});
-    port.postMessage({result:results});
-} 
-
-document.addEventListener('DOMContentLoaded', function() {
-    setDomContent("Search for an artist: ");
 });
