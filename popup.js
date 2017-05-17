3eca2387971237ed7154aaef34b2f887f84c24eb
Tom Griffey <tgriffey9389@gmail.com>
2017-05-17 16:08:14 -0700

    Adjusting Tracked Sites tab to display live session information. Adding a couple utility functions.

--- popup.js
+++ popup.js
@@ -1,11 +1,50 @@
+function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+function countUp(i,appId) {
+    var count = setInterval(function () {
+        i += 1000;
+        document.getElementById(appId+"_active_time").innerHTML = "<b>Active Time:</b> " + msToTime(i);
+    }, 1000);
+}
+
+
 function setAppsOnDom(apps) {
     if (apps.length > 0) {
-        var html=''
+        var html='';
         for (var i = 0; i < apps.length; i++) {
+            var live_session_html = '';
+            if (apps[i].in_session) {
+                // Count a live session as a session
+                apps[i].sessions_today++;
+                apps[i].sessions_today = apps[i].sessions_today + " (in session)";
+                var now = new Date();
+                var active_ms = now - new Date(apps[i].session_start);
+                var active_time = msToTime(active_ms);
+                live_session_html += '<div id="'+apps[i].id+'_active_time"'
+                live_session_html += '<b>Active Time:</b> '
+                live_session_html += active_time
+                live_session_html += '</div>'
+                countUp(active_ms,apps[i].id);
+            }
             html += '<div><b>Name:</b> '+apps[i].name+'</div>'
             html += '<div><b>URL:</b> '
             html += '<a target="_blank" href="https://'+apps[i].url+'">'
             html += apps[i].url+'</a></div>'
+            html += '<div><b>Sessions Today:</b> '+apps[i].sessions_today+'</div>'
+            if (live_session_html.length > 0) {
+                html += live_session_html
+            }
             html += '<hr>'
         };
         document.getElementById('myapps').innerHTML = html;
